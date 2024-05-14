5936ffde419aec44f4c36506ac6f3bfd37be3a55
adelmoneto5292 <145619540+adelmoneto5292@users.noreply.github.com>
2024-05-13 23:33:10 -0300

    Add files via upload

---
+++ Projeto RPG/Personagem.java
@@ -0,0 +1,21 @@
+package gamejava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Personagem {
+    
+    private String nome;
+    private Vida vida;
+    private int ataque;
+    private Classe classe;
+    private int defesa;
+    private int nivel;
+    private int mana;
+    private Inventario inventario;
+    private Armadura armadura;
+    private Habilidade habilidade;
+    private Arma arma;
+
+
+}
