f2e84378d4433e0d4277f1ce84cb45aef3e1ed81
KKaua-git <140828444+KauaProgTester@users.noreply.github.com>
2024-05-13 21:16:30 -0300

    Update Jogador.java

--- Projeto RPG/corpo/Jogador.java
+++ Projeto RPG/corpo/Jogador.java
@@ -3,15 +3,17 @@ package corpo;
 import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
 import main.GamePanel;
 
-public class Jogador extends Corpo{
+public class Jogador extends Corpo {
 
     GamePanel gp;
     KeyHandler keyH;
 
-    public Jogador (GamePanel gp, KeyHandler) {
-        this.gp = gp
+    public Jogador(GamePanel gp, KeyHandler keyH) {
+        
+        this.gp = gp;
         this.keyH = keyH;
+    }
 
-
+    public Jogador(GamePanel gp2, main.KeyHandler keyH2) {
     }
 }
