56f33b04a5db36a61456f2989e6135405c77d85d
adelmoneto5292 <145619540+adelmoneto5292@users.noreply.github.com>
2024-07-02 00:12:51 -0300

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -2,25 +2,26 @@ package src.personagens;
 
 public class Mago extends Personagem {
 
-    public Mago(String nome, int nivel, int saude, int mana) {
-        super(nome, nivel, saude, mana);
+    public Mago(String nome, int vida, String classe, int defesa, 
+    int nivel, int mana, Inventario inventario, Armadura armadura) {
+        
+        super(nome, vida, 8, classe, defesa, nivel, mana, inventario, armadura);        
+
     }
 
-    @Override
     public void atacar(Personagem alvo) {
-        int dano = 12; // Exemplo de dano
-        alvo.setSaude(alvo.getSaude() - dano);
+        int dano = 12; 
+        alvo.setVida(alvo.getVida() - dano);
         System.out.println(getNome() + " lançou um feitiço em " + alvo.getNome() + " causando " + dano + " de dano.");
     }
 
-    @Override
     public void defender() {
         System.out.println(getNome() + " está defendendo.");
     }
 
     public void usarMagia(Personagem alvo) {
-        int danoMagico = 20; // Exemplo de cálculo de dano mágico
-        alvo.setSaude(alvo.getSaude() - danoMagico);
+        int danoMagico = 20; //cálculo de dano mágico
+        alvo.setVida(alvo.getVida() - danoMagico);
         System.out.println(getNome() + " usou magia em " + alvo.getNome() + " causando " + danoMagico + " de dano.");
     }
 }
