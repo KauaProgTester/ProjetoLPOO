b0b084374b30f2fac3956a7e0b9003b1dac2a730
adelmoneto5292 <145619540+adelmoneto5292@users.noreply.github.com>
2024-07-02 00:08:30 -0300

    Update Arqueiro.java

--- Arqueiro.java
+++ Arqueiro.java
@@ -2,25 +2,22 @@ package src.personagens;
 
 public class Arqueiro extends Personagem {
 
-    public Arqueiro(String nome, int nivel, int saude, int mana) {
-        super(nome, nivel, saude, mana);
+    public Guerreiro(String nome, int vida, String classe, int defesa, 
+    int nivel, int mana, Inventario inventario, Armadura armadura) {
+        
+        super(nome, vida, 8, classe, defesa, nivel, mana, inventario, armadura);        
+
     }
 
-    @Override
-    public void atacar(Personagem alvo) {
+    public void ataque(Personagem alvo) {
         int dano = 8; // Exemplo de cálculo de dano
-        alvo.setSaude(alvo.getSaude() - dano);
+        alvo.setVida(alvo.getVida() - dano);
         System.out.println(getNome() + " disparou uma flecha em " + alvo.getNome() + " causando " + dano + " de dano.");
-    }
-
-    @Override
-    public void defender() {
-        System.out.println(getNome() + " está defendendo.");
     }
 
     public void dispararFlechaEspecial(Personagem alvo) {
         int danoEspecial = 18; // Exemplo de cálculo de dano especial
-        alvo.setSaude(alvo.getSaude() - danoEspecial);
+        alvo.setVida(alvo.getVida() - danoEspecial);
         System.out.println(getNome() + " disparou uma flecha especial em " + alvo.getNome() + " causando " + danoEspecial + " de dano.");
     }
 }
