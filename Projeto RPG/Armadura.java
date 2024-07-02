a675c06c22da5d61542989a1d68fb096e5bac3e0
adelmoneto5292 <145619540+adelmoneto5292@users.noreply.github.com>
2024-07-01 23:55:31 -0300

    Update Armadura.java

--- Projeto RPG/Armadura.java
+++ Projeto RPG/Armadura.java
@@ -1,5 +1,20 @@
 package gamejava;
 
-public class Armadura {
+public class Armadura extends Item{
+
+    private int aumentoDefesa; 
+
+    public Armadura(String nome, String descricao, int aumentoDefesa) {
+            super(nome, descricao);
+            this.aumentoDefesa = aumentoDefesa;
+    }
+
+    public int getAumentoDefesa() {
+        return aumentoDefesa;
+    }
+
+    public void setAumentoDefesa(int aumentoDefesa) {
+        this.aumentoDefesa = aumentoDefesa;
+    }
     
 }
