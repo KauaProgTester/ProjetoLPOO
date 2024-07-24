4913c67e166fab367c2f84a36585e7950e7e34f2
KKaua-git <140828444+KauaProgTester@users.noreply.github.com>
2024-07-23 23:06:12 -0300

    Update Chefe.java

--- gamejava/Chefe.java
+++ gamejava/Chefe.java
@@ -2,6 +2,6 @@ package gamejava;
 
 public class Chefe extends Inimigo {
     public Chefe(double x, double y){
-        super("CHEFE", 300, "Ataque poderoso", 25,x,y);
+        super("CHEFE", 300, "Ataque poderoso", 100,x,y);
     }
 }
